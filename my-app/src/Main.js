a1c0d28f14122e85dfc0567c8649e2535339070d
Anna Baranowska <baranowskaanna8@gmail.com>
2021-10-01 17:06:36 +0200

    all

---
+++ my-app/src/Main.js
@@ -0,0 +1,19 @@
+import React from "react";
+import MainView from "./components/MainView";
+
+function App() {
+  return (
+    <body>
+    <div className="mainViewAside">
+      <h2>Martyna Zych</h2>
+        <ul>
+            <li>A. Dvorak</li>
+            <li>B. Smetana</li>
+        </ul>
+    </div>
+      <MainView/>
+    </body>
+  );
+}
+
+export default App;
---
+++ my-app/src/components/MainView.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const MainView = () => {
+    return (
+        <div className="mainView">
+            {/* <div>
+                <h2>Martyna Zych</h2>
+                <ul>
+                    <li>A. Dvorak</li>
+                    <li>B. Smetana</li>
+                </ul>
+            </div> */}
+        </div>
+    )
+}
+
+export default MainView;
