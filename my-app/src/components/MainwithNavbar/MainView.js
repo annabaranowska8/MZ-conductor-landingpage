4f2647af60a1e34bc091567e6bcbf8ccb98eba13
Anna Baranowska <baranowskaanna8@gmail.com>
2021-10-06 00:04:23 +0200

    classes more clear

--- my-app/src/components/MainwithNavbar/MainView.js
+++ my-app/src/components/MainwithNavbar/MainView.js
@@ -1,12 +1,15 @@
 import React from "react";
 import Navbar from "./Navbar";
+import MainFoto from "../../assets/Main.jpg"
 
 const MainView = () => {
     return (
         <>
         <div className="main">
             <div className="main__navbar">
-                <div className="main__navbar--underline" />
+                <div className="main__navbar--background">
+                    <img className="main__navbar--background__img" src={MainFoto}/>                    
+                </div>
                 <Navbar/>
             </div>
             <div className="main__content">
