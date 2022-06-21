e1a8f028566ff1778fe00f3979abc8682a132378
Anna Baranowska <baranowskaanna8@gmail.com>
2022-06-21 18:47:34 +0200

    biography and calendar updated, new multilanguage feature release

--- src/components/MainwithNavbar/Concerts/Concerts.js
+++ src/components/MainwithNavbar/Concerts/Concerts.js
@@ -10,26 +10,26 @@ const Concerts = () => {
                 <h2 className="concerts__container--title">{ t ("title_concerts")}</h2>
                 <ul className="concerts__container--events_list">
                     <li className="single_event_list">
-                        <a href={t ("calendar_website_zlin")} target="_blank">
-                            <div className="event_list_entry event_date">{t ("calendar_date_zlin")}</div>
-                            <div className="event_list_entry event_event">{t ("calendar_name_zlin")}</div>
-                            <div className="event_list_entry event_location">{t ("calendar_place_zlin")}</div>
+                        <a href={t("calendar_website_warsaw_25_02_23")} target="_blank">
+                            <div className="event_list_entry event_date">{t ("calendar_date_warsaw_25_02_23")}</div>
+                            <div className="event_list_entry event_event">{t ("calendar_name_warsaw_25_02_23")}</div>
+                            <div className="event_list_entry event_location">{t ("calendar_place_warsaw_25_02_23")}</div>
                             <div className="event_list_entry event_program">{t ("calendar_more")}</div>
                         </a>       
                     </li>
                     <li className="single_event_list">
-                        <a>
-                            <div className="event_list_entry event_date">{t ("calendar_date_rzeszow")}</div>
-                            <div className="event_list_entry event_event">{t ("calendar_name_rzeszow")}</div>
-                            <div className="event_list_entry event_location">{t ("calendar_place_rzeszow")}</div>
-                            <div className="event_list_entry event_program">{t ("calendar_more_rzeszow")}</div>
+                        <a href={t("calendar_website_karlove_vary_22_09_22")} target="_blank">
+                            <div className="event_list_entry event_date">{t ("calendar_date_karlove_vary_22_09_22")}</div>
+                            <div className="event_list_entry event_event">{t ("calendar_name_karlove_vary_22_09_22")}</div>
+                            <div className="event_list_entry event_location">{t ("calendar_place_karlove_vary_22_09_22")}</div>
+                            <div className="event_list_entry event_program">{t ("calendar_more")}</div>
                         </a>       
                     </li>
                     <li className="single_event_list">
-                        <a href={t("calendar_website_warsaw")} target="_blank">
-                            <div className="event_list_entry event_date">{t ("calendar_date_warsaw")}</div>
-                            <div className="event_list_entry event_event">{t ("calendar_name_warsaw")}</div>
-                            <div className="event_list_entry event_location">{t ("calendar_place_warsaw")}</div>
+                        <a href={t("calendar_website_zlin")} target="_blank">
+                            <div className="event_list_entry event_date">{t ("calendar_date_zlin")}</div>
+                            <div className="event_list_entry event_event">{t ("calendar_name_zlin")}</div>
+                            <div className="event_list_entry event_location">{t ("calendar_place_zlin")}</div>
                             <div className="event_list_entry event_program">{t ("calendar_more")}</div>
                         </a>       
                     </li>
