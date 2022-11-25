51165e011fe4d2e7e59c0f693e4395ba92e6b434
Anna Baranowska <baranowskaanna8@gmail.com>
2022-11-25 23:31:45 +0100

    alice carousel fix

--- src/components/MainwithNavbar/Gallery/Gallery.js
+++ src/components/MainwithNavbar/Gallery/Gallery.js
@@ -43,7 +43,7 @@ const Gallery = () => {
     console.log(pictures);
     setTimeout(() => {
       setPictures(items)
-    }, 200);
+    }, 400);
   })
 
   const nextButton = <FontAwesomeIcon icon={faArrowRight} />;
