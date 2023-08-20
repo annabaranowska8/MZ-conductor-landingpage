00f3c35807401b192d0cb854fa033bbed20a39d2
Anna Baranowska <baranowskaanna8@gmail.com>
2023-08-20 19:19:35 +0200

    concerts update

--- src/components/MainwithNavbar/Concerts/Concerts.js
+++ src/components/MainwithNavbar/Concerts/Concerts.js
@@ -1,62 +1,39 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import concertList from './concertList';
 
 const Concerts = () => {
   const { t } = useTranslation();
+console.log(concertList)
 
   return (
     <div className="concerts" id="concerts">
       <div className="concerts__container">
         <h2 className="concerts__container--title">{t("title_concerts")}</h2>
         <ul className="concerts__container--events_list">
-          <li className="single_event_list">
-            <a href={t("calendar_website_hungary_3-10_09_23")} target="_blank">
-              <div className="event_list_entry event_date">
-                {t("calendar_date_hungary_3-10_09_23")}
-              </div>
-              <div className="event_list_entry event_event">
-                {t("calendar_name_hungary_3-10_09_23")}
-              </div>
-              <div className="event_list_entry event_location">
-                {t("calendar_place_hungary_3-10_09_23")}
-              </div>
-              <div className="event_list_entry event_program">
-                {t("see_more")}
-              </div>
-            </a>
-          </li>
-          <li className="single_event_list">
-            <a href={t("calendar_website_olsztyn_23_02_2024")} target="_blank">
-              <div className="event_list_entry event_date">
-                {t("calendar_date_olsztyn_23_02_2024")}
-              </div>
-              <div className="event_list_entry event_event">
-                {t("calendar_name_olsztyn_23_02_2024")}
-              </div>
-              <div className="event_list_entry event_location">
-                {t("calendar_place_olsztyn_23_02_2024")}
-              </div>
-              <div className="event_list_entry event_program">
-                {t("see_more")}
-              </div>
-            </a>
-          </li>
-          <li className="single_event_list">
-            <a href={t("calendar_website_olsztyn_01_03_2024")} target="_blank">
-              <div className="event_list_entry event_date">
-                {t("calendar_date_olsztyn_01_03_2024")}
-              </div>
-              <div className="event_list_entry event_event">
-                {t("calendar_name_olsztyn_01_03_2024")}
-              </div>
-              <div className="event_list_entry event_location">
-                {t("calendar_place_olsztyn_01_03_2024")}
-              </div>
-              <div className="event_list_entry event_program">
-                {t("see_more")}
-              </div>
-            </a>
-          </li>
+            {concertList.map(concert => (
+                <li className="single_event_list" key={concert.id}>
+                <a href={t(concert.website)} target="_blank">
+                  <div className="event_list_entry event_date">
+                    {t(concert.date)}
+                  </div>
+                  <div className="event_list_entry event_event">
+                    <span>{t(concert.compositor1)}</span>
+                    {concert.compositor2 !== "" && <span>{t(concert.compositor2)}</span>}
+                    {concert.compositor3 !== "" && <span>{t(concert.compositor3)}</span>}
+                  </div>
+                  <div className="event_list_entry event_location">
+                    <span>{t(concert.building)}</span>
+                    <span>{t(concert.country)}</span>
+                  </div>
+                  <div className="event_list_entry event_program">
+                    <div className="see-more-btn">
+                      {t("see_more")}
+                    </div>
+                  </div>
+                </a>
+              </li>
+            ))}
         </ul>
       </div>
     </div>
